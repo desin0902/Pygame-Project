5fc413c4b86a5110e8f2d216f4311b03a5b5cf81
David <desin@uchicago.edu>
2025-10-16 00:08:11 -0500

    Added a parent class for player and enemy classes in sprites.py to inherit from. Should make code more modular and reduce repeated code.

--- spritesheet.py
+++ spritesheet.py
@@ -1,4 +1,35 @@
 import pygame
+from config import *
+
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        self.animation_list = []
+        self.action_state = 0
+        self.frame = 0
+        self.last_update = pygame.time.get_ticks()
+        self.animation_rate = PLAYER_ANIMATION_SPEED
+    
+    def set_action_state(self, new_state, new_rate=None):
+        if self.action_state != new_state:
+            self.action_state = new_state
+            self.frame = 0
+            self.last_update = pygame.time.get_ticks()
+            if new_rate:
+                self.animation_rate = new_rate
+
+    def animate(self):
+        current_time = pygame.time.get_ticks()
+
+        if current_time - self.last_update >= self.animation_rate:
+            self.frame += 1
+            self.last_update = current_time
+
+            if self.frame >= len(self.animation_list[self.action_state]):
+                self.frame = 0
+
+        self.image = pygame.Surface([self.width, self.height], pygame.SRCALPHA)
+        self.image.blit(self.animation_list[self.action_state][self.frame], (0, 0))
 
 class SpriteSheet():
     def __init__(self, image):
@@ -8,5 +39,4 @@ class SpriteSheet():
     def get_image(self, frame, width, height):
         image = pygame.Surface((width, height), pygame.SRCALPHA).convert_alpha()
         image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
-        #image = pygame.transform.scale(image, (width * scale, height * scale))
         return image
